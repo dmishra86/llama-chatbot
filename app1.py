1283f3d97291473b81afa7afb581fbdbb10b65f1
dmishra86 <dmishra86@outlook.com>
2024-06-30 23:50:48 -0500

    update app1.py

--- app1.py
+++ app1.py
@@ -1,10 +1,15 @@
 from langchain_community.llms import Ollama
 from langchain.llms import HuggingFaceHub
 import streamlit as st
+from decouple import config
+import os
+
 
 llm = Ollama(model="llama3")
 st.title("Q&A app using Llama 3")
 
+hf_api_key = config('HUGGINGFACEHUB_API_TOKEN')
+# llm=HuggingFaceHub(repo_id = "nyu-visionx/cambrian-8b")
 
 prompt = st.text_area("Enter your prompt:")
 
